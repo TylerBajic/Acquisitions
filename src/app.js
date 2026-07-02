2f3adcb94900a493906827af0feeb9871e955306
TylerBajic <tylerbajic18@gmail.com>
2026-07-02 15:42:05 -0500

    Implement CI/CD pipelines and GitHub actions

--- src/app.js
+++ src/app.js
@@ -36,4 +36,8 @@ app.use('/api/auth', authRoutes); //
 
 app.use('/api/users', usersRoutes); // Add this line to include the user routes
 
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route Not Found' });
+})
+
 export default app;
